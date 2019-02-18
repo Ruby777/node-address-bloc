161168a2c3df6469ab84b0d32b99b49be1ced473
Lucy <lucynn11@gmail.com>
2019-02-18 19:05:20 +0400

    prompt and menu

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -1,13 +1,51 @@
 const inquirer = require('inquirer');
 
 module.exports = class MenuController {
-    constructor(){}
+    constructor(){
+        this.mainMenuQuestions = [{
+            type: "list",
+            name:"mainMenuChoice",
+            message: "Please choose from an option below:",
+            choices: [
+                "Add new contact",
+                "Exit"
+            ]
+        }
+     ];
+     this.contacts = [];
+    }
 
     main(){
-        console.log('hello from main');
+        console.log('Welcome to AddressBloc!');
+        inquirer.prompt(this.mainMenuQuestions).then((response) =>{
+            switch(response.mainMenuChoice){
+                case "Add new contact":
+                    this.addContact();
+                    break;
+                case "Exit":
+                    this.exit();
+                default:
+                    console.log("Invalid Input");
+                    this.main();  
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
     }
 
     clear(){
         console.log("\x1Bc");
+    }
+
+    addContact(){
+        this.clear();
+        console.log('addContact called');
+        this.main();
+    }
+
+    exit(){
+        console.log("Thank you for using AddressBloc");
+        process.exit();
     }
 }
